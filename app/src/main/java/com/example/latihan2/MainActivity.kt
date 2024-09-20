b1643f3130708519fb3bd2ea616d17cf0ce0f015
AndriEnd <ndrian1710@gmail.com>
2024-09-20 17:09:48 +0700

    update

--- app/src/main/java/com/example/latihan2/MainActivity.kt
+++ app/src/main/java/com/example/latihan2/MainActivity.kt
@@ -1,35 +1,36 @@
 package com.example.latihan2
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnMoveActivity: Button = findViewById(R.id.btn_more_activity)
-        btnMoveActivity.setOnClickListener {
-            val moveIntent = Intent(this@MainActivity, page2::class.java)
-            startActivity(moveIntent)
-        }
+        btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveWithDataActivity.setOnClickListener(this)
     }
-}
 
-
-    fun onClick(v: View?) {
-        when(v?.id){
+    override fun onClick(v: View?) {
+        when (v?.id) {
             R.id.btn_more_activity -> {
+                val moveIntent = Intent(this@MainActivity, page2::class.java)
+                startActivity(moveIntent)
+            }
+            R.id.btn_move_activity_data -> {
+                val moveIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                startActivity(moveIntent)
             }
         }
     }
-
-private fun Button.setOnClickListener(mainActivity: MainActivity) {
-    TODO("Not yet implemented")
 }
+
