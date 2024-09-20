b9d4097acdaa7b238c93cfd1c2485a1a672fd151
AndriEnd <ndrian1710@gmail.com>
2024-09-21 03:01:11 +0700

    latihan moveIntentWithData Done

--- app/src/main/java/com/example/latihan2/MainActivity.kt
+++ app/src/main/java/com/example/latihan2/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnMoveActivity: Button = findViewById(R.id.btn_more_activity)
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
         btnMoveActivity.setOnClickListener(this)
 
         val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
@@ -22,12 +22,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.btn_more_activity -> {
+            R.id.btn_move_activity -> {
                 val moveIntent = Intent(this@MainActivity, page2::class.java)
                 startActivity(moveIntent)
             }
             R.id.btn_move_activity_data -> {
                 val moveIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveIntent.putExtra(MoveWithDataActivity.EXTRA_NAME,"Mamank Racing")
+                moveIntent.putExtra(MoveWithDataActivity.EXTRA_AGE,25)
                 startActivity(moveIntent)
             }
         }
