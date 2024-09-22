48e7d67f23ab9589b306c7fd99454f8326222863
AndriEnd <ndrian1710@gmail.com>
2024-09-22 23:59:15 +0700

    activity_move_with_Object Done !

--- app/src/main/java/com/example/latihan2/MainActivity.kt
+++ app/src/main/java/com/example/latihan2/MainActivity.kt
@@ -5,19 +5,25 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.sql.RowId
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+// Button Activity
         val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
         btnMoveActivity.setOnClickListener(this)
-
+//Button Move Activity With data
         val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
         btnMoveWithDataActivity.setOnClickListener(this)
+//Button move activity with object
+        val btnMoveWithObject:Button = findViewById(R.id.btn_move_activity_object)
+        btnMoveWithObject.setOnClickListener(this)
+
     }
 
     override fun onClick(v: View?) {
@@ -28,9 +34,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
             R.id.btn_move_activity_data -> {
                 val moveIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
-                moveIntent.putExtra(MoveWithDataActivity.EXTRA_NAME,"Mamank Racing")
-                moveIntent.putExtra(MoveWithDataActivity.EXTRA_AGE,25)
+                moveIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Mamank Racing")
+                moveIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 25)
                 startActivity(moveIntent)
+            }
+            R.id.btn_move_activity_object ->{
+                val person =Person(
+                    "MAMANK RACING",
+                    5,
+                    "ABACOT.@gmail.com",
+                    "lampung"
+                )
+
+                val moveWithObjectintent = Intent(this@MainActivity,MoveWithObjectActivity::class.java)
+                moveWithObjectintent.putExtra(MoveWithObjectActivity.EXTRA_PERSON,person)
+                startActivity(moveWithObjectintent)        
             }
         }
     }
